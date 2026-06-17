1bbb197af4113e7ee2df0c01787cbae9a68ac74c
Sandun Pushpika <sandun.pushpika123@gmail.com>
2026-06-18 01:12:00 +0530

    Added searching filters

--- Core/DTOs/Request/Vehicle/VehicleSearchRequest.cs
+++ Core/DTOs/Request/Vehicle/VehicleSearchRequest.cs
@@ -11,5 +11,7 @@ public class VehicleSearchRequest
     public int? MinPassengers { get; set; }
     public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 10;
+    public DateTime? FromDate { get; set; }
+    public DateTime? ToDate { get; set; }
 }
 
