050dad4ff3936d1c402876535832f3b01181414b
Chathuri Dhananjana <2002chathudhana21@gmail.com>
2026-06-27 22:45:38 +0530

    getAllBookings implementation with FE (#9)
    * getAllBookings implementation with FE
    
    * Issues fixed

--- Application/Interfaces/Repositories/IBookingRepository.cs
+++ Application/Interfaces/Repositories/IBookingRepository.cs
@@ -1,6 +1,6 @@
+using Core.DTOs.Request.Bookings;
 using Core.DTOs.Response;
 using Core.Entities;
-
 namespace Application.Interfaces.Repositories;
 
 public interface IBookingRepository
@@ -8,7 +8,7 @@ public interface IBookingRepository
     Task AddBooking(Booking booking);
     Task<Booking?> GetBookingById(int id);
     Task UpdateBooking(Booking booking);
-    Task<IReadOnlyCollection<Booking>> GetAllBookings();
+    Task<(IReadOnlyCollection<Booking> Bookings,int Total)>  GetAllBookings(BookingSearchRequest request);
     Task DeleteBooking(Booking booking);
     Task<bool> IsBooked(int vehicleId, DateTime from, DateTime to);
     Task<IReadOnlyCollection<BookedDateRangeResponse>> GetBookedDatesByVehicleId(int vehicleId);
